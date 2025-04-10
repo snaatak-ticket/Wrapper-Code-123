556c7e242b86e288fadad845510bef7a0dd5a089
Aayush Verma <aayush.verma@mygurukulam.co>
2025-04-10 22:35:24 +0530

    Update backend.tf

--- Env/Dev/Network-Skeleton/backend.tf
+++ Env/Dev/Network-Skeleton/backend.tf
@@ -1,8 +1,8 @@
-#terraform {
-#  backend "s3" {
-#    bucket         = "otms-tf-state-bucket"
-#    key            = "env/dev/module/network-skeleton/terraform.tfstate"
-#    region         = "us-east-2"
-#    dynamodb_table = "dev-otms-tf-lock-table"
-#  }
-#}
+terraform {
+  backend "s3" {
+    bucket         = "otms-tf-state-bucket"
+    key            = "env/dev/module/network-skeleton/terraform.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "dev-otms-tf-lock-table"
+  }
+}
