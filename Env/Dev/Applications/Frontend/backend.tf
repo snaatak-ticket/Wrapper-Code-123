dbe8da37d17c374c318ae0c82ad1add6792f5ca2
mohitmaikhuri03 <mkmaikhuri@gmail.com>
2025-04-11 01:05:55 +0530

    updated code

---
+++ Env/Dev/Applications/Frontend/backend.tf
@@ -0,0 +1,9 @@
+
+terraform {
+  backend "s3" {
+    bucket         = "otms-sprint5-bucket"
+    key            = "env/dev/Applications/Frontend/terraform.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "dev-otms-tf-lock-table"
+  }
+}
---
+++ Env/Dev/Applications/Frontend/data.tf
@@ -0,0 +1,16 @@
+data "terraform_remote_state" "network" {
+  backend = "s3"
+
+  config = {
+    bucket = "otms-sprint5-bucket"
+    key    = "env/dev/module/network-skeleton/terraform.tfstate"
+    region = "us-east-2"
+  }
+}
+
+data "aws_ami" "frontend_ami" {
+  filter {
+    name   = "name"
+    values = ["otms-frontend-ami"]
+  }
+}
