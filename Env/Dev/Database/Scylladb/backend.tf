977545e0d22de309c857b17679aae16e09956bd6
rohitchopra-mygurukulam <rohit.chopra@mygurukulam.co>
2025-04-10 07:58:48 +0530

    Update backend.tf

--- Env/Dev/Database/Scylladb/backend.tf
+++ Env/Dev/Database/Scylladb/backend.tf
@@ -1,1 +1,8 @@
-
+terraform {
+  backend "s3" {
+    bucket         = "otms-tf-state-bucket"
+    key            = "env/dev/module/database/scylladb/terraform.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "dev-otms-tf-lock-table"
+  }
+}
