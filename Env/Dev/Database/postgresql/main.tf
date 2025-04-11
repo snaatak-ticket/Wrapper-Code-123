f5c2b1e0f04f1513f52661f0ffa2dc86902a45e5
Ramratan101 <ram.yadav@mygurukulam.co>
2025-04-11 11:45:12 +0530

    Create main.tf

---
+++ Env/Dev/Database/postgresql/main.tf
@@ -0,0 +1,17 @@
+module "postgresql" {
+  source = "git::git@github.com:snaatak-Zero-Downtime-Crew/Tf-Modules.git//Standalone-VM?ref=Prashant-SCRUM-345"
+
+  count               = var.number_of_instances
+  project_name        = var.project_name
+  instance_name       = var.instance_name
+  ami_id              = data.aws_ami.postgresql_ami.id
+  instance_type       = var.instance_type
+  key_name            = var.key_name
+  subnet_id           = data.terraform_remote_state.otms_vpc.outputs.subnet_ids.dev-otms-database-subnet
+  security_groups     = [data.terraform_remote_state.otms_vpc.outputs.all_sg_ids.postgresql]
+  env                 = var.env
+  number_of_instances = var.number_of_instances
+  aws_region          = var.aws_region
+  #owner               = var.owner      # only if the module supports this
+  #tag_name            = local.tag_name # only if the module supports this
+}
