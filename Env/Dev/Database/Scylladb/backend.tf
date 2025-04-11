1ca52ab90f2d6befd27c4275cc65dfe090a0ae92
rohitchopra-mygurukulam <rohit.chopra@mygurukulam.co>
2025-04-11 06:13:50 +0530

    Delete Env/Dev/Database/Scylladb/backend.tf

--- Env/Dev/Database/Scylladb/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "otms-tf-state-bucket"
-    key            = "env/dev/module/database/scylladb/terraform.tfstate"
-    region         = "us-east-2"
-    dynamodb_table = "dev-otms-tf-lock-table"
-  }
-}
