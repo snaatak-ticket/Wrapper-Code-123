a946dda6307a96c2134eddd98eb41b33ff56dc11
Anuj6392 <anuj.yadav@mygurukulam.co>
2025-04-12 11:25:19 +0530

    Update data.tf

--- ENV/DEV/Applications/Attendance/data.tf
+++ ENV/DEV/Applications/Attendance/data.tf
@@ -1,1 +1,33 @@
+data "terraform_remote_state" "otms_vpc" {
+  backend = "s3"
 
+  config = {
+    bucket         = "otms-sprint5-bucket"
+    key            = "env/qa/module/network-skeleton/terraform.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "dev-otms-tf-lock-table"
+  }
+}
+
+
+data "aws_ami" "attendance_ami" {
+  most_recent = true
+  owners      = ["self"]
+
+  filter {
+    name   = "name"
+    values = ["dev-otms-attendance-ami"]
+  }
+}
+
+
+data "terraform_remote_state" "frontend" {
+  count   = var.enable_http_listener ? 0 : 1
+  backend = "s3"
+
+  config = {
+    bucket = "otms-sprint5-bucket"
+    key    = "env/qa/Applications/Frontend/terraform.tfstate"
+    region = "us-east-2"
+  }
+}
