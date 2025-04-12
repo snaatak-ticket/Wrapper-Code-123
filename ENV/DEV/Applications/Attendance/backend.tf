5a81521791ae8fb8e6423512a92f13e997cd8b0f
Anuj6392 <anuj.yadav@mygurukulam.co>
2025-04-12 17:47:38 +0530

    Update backend.tf

--- ENV/DEV/Applications/Attendance/backend.tf
+++ ENV/DEV/Applications/Attendance/backend.tf
@@ -1,7 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "otms-sprint5-bucket"
-    key    = "env/qa/network skeleton/terraform.tfstate"
-    region = "us-east-2"
+    bucket         = "otms-sprint5-bucket"
+    key            = "env/qa/Applications/Attendance/terraform.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "dev-otms-tf-lock-table"
   }
 }
