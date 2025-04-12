76d4521922ef8a58760f544246e0d67d1cced874
shashiprabhayadav <shashi.prabha@mygurukulam.co>
2025-04-12 14:35:46 +0530

    frontend-qa wrapper code

---
+++ Env/QA/Applications/Frontend/data.tf
@@ -0,0 +1,14 @@
+data "terraform_remote_state" "network" {
+  backend = "s3"
+  config = {
+    bucket = "otms-sprint5-bucket"
+    key    = "env/qa/module/network-skeleton/terraform.tfstate"
+    region = "us-east-2"
+  }
+}
+data "aws_ami" "frontend_ami" {
+  filter {
+    name   = "name"
+    values = ["dev-otms-frontend-ami"]
+  }
+}
---
+++ Env/QA/Applications/Frontend/output.tf
@@ -0,0 +1,24 @@
+output "asg_name" {
+  value = module.qa_frontend_infra.asg_name
+}
+output "launch_template_id" {
+  value = module.qa_frontend_infra.launch_template_id
+}
+output "target_group_arn" {
+  value = module.qa_frontend_infra.target_group_arn
+}
+output "launch_template_name" {
+  value = module.qa_frontend_infra.launch_template_name
+}
+output "listener_rule_arn" {
+  value = module.qa_frontend_infra.listener_rule_arn
+}
+output "target_group_name" {
+  value = module.qa_frontend_infra.target_group_name
+}
+output "target_group_id" {
+  value = module.qa_frontend_infra.target_group_id
+}
+output "alb_lr_arn" {
+  value = module.qa_frontend_infra.alb_lr_arn
+}
