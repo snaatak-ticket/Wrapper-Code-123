63102975a69bb505375fb174c46450a7a6d092c3
sharvarikhamkar1304 <sharvari.khamkar@mygurukulam.co>
2025-04-12 15:33:16 +0530

    Create output.tf

---
+++ Env/Dev/Middleware/Redis/output.tf
@@ -0,0 +1,13 @@
+output "instance_public_ip" {
+  description = "Public IPs of the redis EC2 instances"
+  value       = module.redis[*].instance_public_ip
+}
+
+output "instance_private_ip" {
+  description = "The public IP of the EC2 instance"
+  value       = module.redis[*].instance_private_ip
+}
+output "instance_id" {
+  description = "The ID of the EC2 instance"
+  value       = module.redis[*].instance_id
+}
