1c17650ab8325257f510e1cf34f30d3c465261e6
sharvarikhamkar1304 <sharvari.khamkar@mygurukulam.co>
2025-04-12 15:32:48 +0530

    Create main.tf

---
+++ Env/Dev/Middleware/Redis/main.tf
@@ -0,0 +1,20 @@
+module "redis" {
+  source = "git::git@github.com:snaatak-Zero-Downtime-Crew/Tf-Modules.git//Standalone-VM?ref=Prashant-SCRUM-345"
+
+  # count                 = tonumber(var.number_of_instances)
+  project_name          = var.project_name
+  instance_name         = var.instance_name
+  ami_id                = data.aws_ami.redis_ami.id
+  instance_type         = var.instance_type
+  key_name              = var.key_name
+  subnet_id             = data.terraform_remote_state.otms_vpc.outputs.subnet_ids.dev-otms-database-subnet
+  security_groups       = [data.terraform_remote_state.otms_vpc.outputs.all_sg_ids.redis]
+  env                   = var.env
+  number_of_instances   = var.number_of_instances
+  aws_region            = var.aws_region
+  owner_name            = var.owner
+  device_name           = "/dev/sda1"
+  ebs_volume_size       = "8"
+  ebs_volume_type       = "gp2"
+  delete_on_termination = true
+}
