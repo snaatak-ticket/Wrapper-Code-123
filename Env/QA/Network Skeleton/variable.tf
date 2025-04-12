ecff5cc54d540cfac4748de618c37ec5d0b069d9
SheetalNain <sheetal.nain@mygurukulam.co>
2025-04-12 13:00:54 +0530

    Update variable.tf

--- Env/QA/Network Skeleton/variable.tf
+++ Env/QA/Network Skeleton/variable.tf
@@ -1,7 +1,7 @@
 #################### VPC ########################
 variable "region" {
   type        = string
-  default     = "us-east-1"
+  default     = "us-east-2"
   description = "enter region name"
 }
 variable "env" {
@@ -64,7 +64,7 @@ variable "subnet_cidrs" {
 variable "subnet_azs" {
   description = "List of availability zones for subnets"
   type        = list(string)
-  default     = ["us-east-1a", "us-east-1a", "us-east-1a", "us-east-1a", "us-east-1b"]
+  default     = ["us-east-2a", "us-east-2a", "us-east-2a", "us-east-2a", "us-east-2b"]
 }
 
 #################### NAT ########################
