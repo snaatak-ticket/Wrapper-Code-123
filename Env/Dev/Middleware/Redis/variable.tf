d16bcf1d85bb4e96ef84ee1608afa1448c74d7e8
sharvarikhamkar1304 <sharvari.khamkar@mygurukulam.co>
2025-04-12 15:36:14 +0530

    Create variable.tf

---
+++ Env/Dev/Middleware/Redis/variable.tf
@@ -0,0 +1,45 @@
+variable "aws_region" {
+  type        = string
+  default     = "us-east-2"
+  description = "enter region name"
+}
+
+variable "number_of_instances" {
+  description = "Number of EC2 instances to launch"
+  type        = number
+  default     = 1
+}
+
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.medium"
+}
+
+variable "key_name" {
+  description = "Key pair name for SSH access"
+  type        = string
+  default     = "otms_dev"
+}
+
+####tags###########
+variable "env" {
+  type        = string
+  description = "enter env name"
+}
+
+variable "project_name" {
+  description = "Project name identifier"
+  type        = string
+
+}
+variable "instance_name" {
+  description = "Project name identifier"
+  type        = string
+}
+
+variable "owner" {
+  description = "Project creater identifier"
+  type        = string
+}
