af4aee47d1407be8e797b805528db11d72b6e60a
sharvarikhamkar1304 <sharvari.khamkar@mygurukulam.co>
2025-04-12 19:18:52 +0530

    Create data.tf

---
+++ Env/QA/Middleware/Redis/data.tf
@@ -0,0 +1,18 @@
+data "terraform_remote_state" "otms_vpc" {
+  backend = "s3"
+
+  config = {
+    bucket         = "otms-sprint5-bucket"
+    key            = "env/qa/module/network-skeleton/terraform.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "dev-otms-tf-lock-table"
+  }
+}
+
+
+data "aws_ami" "redis_ami" {
+  filter {
+    name   = "name"
+    values = ["dev-otms-redis-ami"]
+  }
+}
