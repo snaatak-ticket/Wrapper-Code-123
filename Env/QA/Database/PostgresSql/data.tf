77eddd6227e4a65dbdb10686b3480d5891e44fd0
Shubhanshi-2123 <shubhanshi.suryal@mygurukulam.co>
2025-04-13 13:02:06 +0530

    Update data.tf

--- Env/QA/Database/PostgresSql/data.tf
+++ Env/QA/Database/PostgresSql/data.tf
@@ -1,19 +1,19 @@
-# Reading outputs from previously deployed VPC/networking resources
+
 
 data "terraform_remote_state" "otms_vpc" {
   backend = "s3"
 
   config = {
     bucket       = "otms-sprint5-bucket"
-    key          = "env/qa/module/network-skeleton/terraform.tfstate"  #path to specific tfstate file
+    key          = "env/qa/module/network-skeleton/terraform.tfstate"  
     region       = "us-east-2"
     use_lockfile = true    # here using lockfile instead of deprecated dynamodb table
   }
 }
 
 data "aws_ami" "postgresql" {
-  most_recent = true    #  Always get the latest matching AMI
-  owners      = ["self"]  #ami id from my current aws account only
+  most_recent = true   
+  owners      = ["self"] 
 
   filter {
     name   = "name"
