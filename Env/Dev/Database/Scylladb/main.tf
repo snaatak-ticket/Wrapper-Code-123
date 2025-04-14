80a1830e61264de3ac79fdc77c260a93e3292da5
rohitchopra-mygurukulam <rohit.chopra@mygurukulam.co>
2025-04-14 11:21:39 +0530

    Update main.tf

--- Env/Dev/Database/Scylladb/main.tf
+++ Env/Dev/Database/Scylladb/main.tf
@@ -1,18 +1,20 @@
 module "scylladb" {
   source = "git::git@github.com:snaatak-Zero-Downtime-Crew/Tf-Modules.git//Standalone-VM?ref=Prashant-SCRUM-345"
 
-  count               = var.number_of_instances
-  project_name        = var.project_name
-  instance_name       = var.instance_name
-  ami_id              = data.aws_ami.scylla_ami.id
-  instance_type       = var.instance_type
-  key_name            = var.key_name
-  subnet_id           = data.terraform_remote_state.otms_vpc.outputs.subnet_ids.dev-otms-database-subnet
-  security_groups     = [data.terraform_remote_state.otms_vpc.outputs.all_sg_ids.scylla]
-  env                 = var.env
-  number_of_instances = var.number_of_instances
-  aws_region          = var.aws_region
-  #owner               = var.owner      # only if the module supports this
-  #tag_name            = local.tag_name # only if the module supports this
+  count                 = var.number_of_instances
+  project_name          = var.project_name
+  instance_name         = var.instance_name
+  ami_id                = data.aws_ami.scylla_ami.id
+  instance_type         = var.instance_type
+  key_name              = var.key_name
+  subnet_id             = data.terraform_remote_state.otms_vpc.outputs.subnet_ids.dev-otms-database-subnet
+  security_groups       = [data.terraform_remote_state.otms_vpc.outputs.all_sg_ids.scylla]
+  env                   = var.env
+  number_of_instances   = var.number_of_instances
+  aws_region            = var.aws_region
+  owner_name            = var.owner # only if the module supports this
+  device_name           = "/dev/sda1"
+  ebs_volume_size       = "20"
+  ebs_volume_type       = "gp2"
+  delete_on_termination = true
 }
-
