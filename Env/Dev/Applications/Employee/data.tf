3dd92eb15f02708e9d88181f35c4c5a833898ddb
PrashantMgklm <prashant.sharma@mygurukulam.co>
2025-04-14 14:33:20 +0530

    Create data.tf

---
+++ Env/Dev/Applications/Employee/data.tf
@@ -0,0 +1,27 @@
+data "terraform_remote_state" "otms_ns" {
+  backend = "s3"
+
+  config = {
+    bucket = "otms-sprint5-bucket"
+    key    = "env/dev/module/network-skeleton/terraform.tfstate"
+    region = "us-east-2"
+  }
+}
+
+data "aws_ami" "employee_ami" {
+  filter {
+    name   = "name"
+    values = ["dev-otms-employee-ami"]
+  }
+}
+
+data "terraform_remote_state" "frontend" {
+  count   = var.enable_http_listener ? 0 : 1
+  backend = "s3"
+
+  config = {
+    bucket = "otms-sprint5-bucket"
+    key    = "env/dev/Applications/Frontend/terraform.tfstate"
+    region = "us-east-2"
+  }
+}
