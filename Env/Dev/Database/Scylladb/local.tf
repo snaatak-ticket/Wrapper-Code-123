7604711477e6aa8a512f1ff59764aa8020271065
rohitchopra-mygurukulam <rohit.chopra@mygurukulam.co>
2025-04-14 11:21:09 +0530

    Update local.tf

--- Env/Dev/Database/Scylladb/local.tf
+++ Env/Dev/Database/Scylladb/local.tf
@@ -1,7 +1,7 @@
 locals {
   common_tags = {
     env = var.env
-    #owner = var.owner
-    #tag_name = "${var.env}-${var.project_name}-${var.instance_name}"
+    owner = var.owner
+    tag_name = "${var.env}-${var.project_name}-${var.instance_name}_ec2"
   }
 }
