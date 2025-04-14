77295280981d78e4a7f7debacd990813df2ed2c3
rohitchopra-mygurukulam <rohit.chopra@mygurukulam.co>
2025-04-14 11:18:38 +0530

    Update backend.tf

--- Env/QA/Applications/Salary/backend.tf
+++ Env/QA/Applications/Salary/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "otms-sprint5-bucket"
-    key            = "env/qa/Applications/Salary/terraform.tfstate"
+    key            = "env/dev/module/database/scylladb/terraform.tfstate"
     region         = "us-east-2"
     dynamodb_table = "dev-otms-tf-lock-table"
   }
