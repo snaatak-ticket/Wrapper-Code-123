0a074081ca11beba2a86e06b6fa0b8ad75396cf3
Ramratan101 <ram.yadav@mygurukulam.co>
2025-04-14 10:58:21 +0530

    Update variable.tf

--- Env/Dev/Database/postgresql/variable.tf
+++ Env/Dev/Database/postgresql/variable.tf
@@ -27,15 +27,6 @@ variable "key_name" {
   default     = "otms_dev"
 }
 
-# variable "subnet_id" {
-#   description = "Subnet ID where the EC2 instance will be launched"
-#   type        = string
-# }
-
-# variable "security_groups" {
-#   description = "List of security group IDs"
-#   type        = list(string)
-# }
 
 ####tags###########
 variable "env" {
@@ -53,7 +44,3 @@ variable "instance_name" {
   type        = string
 }
 
-# variable "owner" {
-#   description = "Project creater identifier"
-#   type        = string
-# }
