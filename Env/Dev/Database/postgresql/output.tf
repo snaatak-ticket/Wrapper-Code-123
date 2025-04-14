6245c3d2664347bb80a0180ed15dffedc55072f6
Ramratan101 <ram.yadav@mygurukulam.co>
2025-04-14 11:01:14 +0530

    Update output.tf

--- Env/Dev/Database/postgresql/output.tf
+++ Env/Dev/Database/postgresql/output.tf
@@ -1,1 +1,13 @@
+output "instance_public_ip" {
+  description = "Public IPs of the postgresql EC2 instances"
+  value       = module.postgresql[*].instance_public_ip
+}
 
+output "instance_private_ip" {
+  description = "The public IP of the EC2 instance"
+  value       = module.postgresql[*].instance_private_ip
+}
+output "instance_id" {
+  description = "The ID of the EC2 instance"
+  value       = module.postgresql[*].instance_id
+}
