84ccc4c73bc6f125c64ead195c59f8f796a75164
Aayush Verma <aayush.verma@mygurukulam.co>
2025-04-17 01:02:34 +0530

    Update backend.tf

--- Env/Dev/Network-Skeleton/backend.tf
+++ Env/Dev/Network-Skeleton/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "otms-sprint5-bucket"
+    bucket         = "prod-otms-bucket"
     key            = "env/dev/module/network-skeleton/terraform.tfstate"
     region         = "us-east-2"
     dynamodb_table = "dev-otms-tf-lock-table"
