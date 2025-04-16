64cf801b6c2368e9a09ff5964706603f38f24c5c
pravesh1204 <pravesh.kumar@mygurukulam.co>
2025-04-16 09:16:47 +0530

    Create data.tf

---
+++ Env/Dev/Applications/OpenVpn/data.tf
@@ -0,0 +1,10 @@
+data "terraform_remote_state" "otms_vpc" {
+  backend = "s3"
+
+  config = {
+    bucket = "otms-sprint5-bucket"
+    key    = "env/dev/module/network-skeleton/terraform.tfstate"
+    region = "us-east-2"
+    dynamodb_table = "dev-otms-tf-lock-table"
+  }
+}
