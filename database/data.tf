58e303bad681520ddd61a8fae058384a88e6b29e
aman-dw00 <aman.raj@mygurukulam.co>
2025-04-17 19:55:44 +0530

    Update data.tf

--- database/data.tf
+++ database/data.tf
@@ -1,31 +1,3 @@
-# Redis AMI
-data "aws_ami" "redis" {
-  filter {
-    name   = "name"
-    values = ["Redis updated"]
-  }
-
-}
-
-# Postgres AMI
-data "aws_ami" "postgres" {
-  filter {
-    name   = "name"
-    values = ["Postgres updated"]
-  }
-
-}
-
-# Scylla AMI
-data "aws_ami" "scylla" {
-  filter {
-    name   = "name"
-    values = ["Scylladb ami"]
-  }
-
-}
-
-
 data "terraform_remote_state" "network" {
   backend = "s3"
 
