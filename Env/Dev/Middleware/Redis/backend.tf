e8fbe4c957af3442280449761c417ff734d56398
sharvarikhamkar1304 <sharvari.khamkar@mygurukulam.co>
2025-04-18 14:39:18 +0530

    Update backend.tf

--- Env/Dev/Middleware/Redis/backend.tf
+++ Env/Dev/Middleware/Redis/backend.tf
@@ -6,3 +6,23 @@ terraform {
     dynamodb_table = "dev-otms-tf-lock-table"
   }
 }
+
+data "terraform_remote_state" "otms_vpc" {
+  backend = "s3"
+
+  config = {
+    bucket         = "otms-sprint5-bucket"
+    key            = "env/dev/module/network-skeleton/terraform.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "dev-otms-tf-lock-table"
+  }
+}
+
+data "aws_ami" "redis_ami" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = [var.ami_name_filter]
+  }
+  owners = ["self"]
+}
