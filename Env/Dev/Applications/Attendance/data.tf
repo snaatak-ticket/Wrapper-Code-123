3107a39b2a0f0d9bccb61204a24fdc3867614024
Mohit000003 <mohit.kumar@mygurukulam.co>
2025-04-18 13:35:29 +0530

    Update data.tf

--- Env/Dev/Applications/Attendance/data.tf
+++ Env/Dev/Applications/Attendance/data.tf
@@ -11,7 +11,7 @@ data "terraform_remote_state" "otms_ns" {
 data "aws_ami" "attendance_ami" {
   filter {
     name   = "name"
-    values = ["attendance_ami"]
+    values = ["attendance ami"]
   }
 }
 
