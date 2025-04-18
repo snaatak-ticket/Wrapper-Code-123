088381222fa587c6c4d2f76936c4680fcafda266
sharvarikhamkar1304 <sharvari.khamkar@mygurukulam.co>
2025-04-18 14:54:23 +0530

    Update output.tf

--- Env/QA/Middleware/Redis/output.tf
+++ Env/QA/Middleware/Redis/output.tf
@@ -7,7 +7,9 @@ output "instance_private_ip" {
   description = "The public IP of the EC2 instance"
   value       = module.redis[*].instance_private_ip
 }
+
 output "instance_id" {
   description = "The ID of the EC2 instance"
   value       = module.redis[*].instance_id
 }
+
