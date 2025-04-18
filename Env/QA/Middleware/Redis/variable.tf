5528d66c5ff178f25af2c00c826cf7018c0e8854
sharvarikhamkar1304 <sharvari.khamkar@mygurukulam.co>
2025-04-18 14:56:44 +0530

    Update variable.tf

--- Env/QA/Middleware/Redis/variable.tf
+++ Env/QA/Middleware/Redis/variable.tf
@@ -1,43 +1,64 @@
 variable "aws_region" {
   type        = string
-  default     = "us-east-2"
-  description = "enter region name"
+  description = "Enter region name"
 }
 
 variable "number_of_instances" {
   description = "Number of EC2 instances to launch"
   type        = number
-  default     = 1
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t2.micro"
 }
 
 variable "key_name" {
   description = "Key pair name for SSH access"
   type        = string
-  default     = "otms_dev"
 }
 
 variable "env" {
   type        = string
-  description = "enter env name"
+  description = "Environment name"
 }
 
 variable "project_name" {
   description = "Project name identifier"
   type        = string
-
 }
+
 variable "instance_name" {
-  description = "Project name identifier"
+  description = "Instance name identifier"
   type        = string
 }
 
 variable "owner" {
-  description = "Project creater identifier"
+  description = "Project owner identifier"
+  type        = string
+}
+
+variable "device_name" {
+  description = "EBS device name"
+  type        = string
+}
+
+variable "ebs_volume_size" {
+  description = "EBS volume size in GB"
+  type        = number
+}
+
+variable "ebs_volume_type" {
+  description = "EBS volume type"
+  type        = string
+}
+
+variable "delete_on_termination" {
+  description = "Delete volume on instance termination"
+  type        = bool
+}
+
+variable "ami_name_filter" {
+  description = "Filter value for AMI lookup"
   type        = string
 }
